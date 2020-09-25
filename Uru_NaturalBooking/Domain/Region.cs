bbb1e2a9d97a6497e80b5376dc6b2cd847ec3a92
Joaquin Lamela <joaquin.lamela00@gmail.com>
2020-09-25 18:14:39 -0300

    Adding the class for the regions

---
+++ Uru_NaturalBooking/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public enum RegionName { Región_Metropolitana, Región_Centro_Sur, Región_Este, Región_Literal_Norte, Región_Corredor_Pajaros_Pintados }
+
+        public RegionName Name { get; set; }
+    }
+}
