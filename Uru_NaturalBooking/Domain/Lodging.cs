edb9e2be02ae17a02d1ec9a897211dc0388894ea
Agustin Hernandorena <agustinhernandorena@gmail.com>
2020-09-25 19:06:01 -0300

    Introduce some classes of domain

---
+++ Uru_NaturalBooking/Domain/Lodging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Lodging
+    {
+        public string Name { get; set; }
+
+        public int QuantityOfStars { get; set; }
+
+        public string Address { get; set; }
+
+        public byte [] Images { get; set; }
+        
+        public double PricePerNight { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public virtual TouristSpot TouristSpot { get; set; }
+
+
+    }
+}
---
+++ Uru_NaturalBooking/Domain/Reserve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Reserve
+    {
+        
+        public int Id { get; set;}
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string DescriptionOfGuest { get; set; }
+
+        public virtual Lodging LodgingOfReserve { get; set; }
+
+
+
+
+    }
+}
